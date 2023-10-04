28541ba0fed06cbb9939b40dd6aba3d895b38ee3
Cristobal Selame <cristobal.selame@sfcm.edu>
2023-10-04 10:57:59 -0300

    fechas

---
+++ fechas.js
@@ -0,0 +1,13 @@
+ const fechaActual = new Date();
+
+
+const cumpleaños = new Date('1995-09-07T12:00:00');
+console.log(cumpleaños);
+
+console.log(fechaActual.toString());
+console.log(fechaActual.toDateString());
+console.log(fechaActual.toTimeString());
+
+console.log(cumpleaños.getMonth());
+console.log(cumpleaños.getDate());
+console.log(cumpleaños.getYear());
--- localStorage.js
+++ localStorage.js
@@ -2,3 +2,14 @@ document.getElementById('boton1').addEventListener('click', () => {
   const nombre = prompt('Escribe tu Nombre');
   window.localStorage.setItem('usuario', nombre);
 });
+
+
+document.getElementById('boton2').addEventListener('click', () => {
+  window.localStorage.removeItem('usuario');
+});
+
+if(window.localStorage.usuario){
+  console.log('Hola', window.localStorage.usuario )
+} else {
+  console.log('Hola Anonimo')
+};
